7d01f730124f08fe8d89a875312c948b0c0f0510
OsipovIvan <osipovivan23@gmail.com>
2021-03-21 20:04:56 +0300

    ДЗ к занятию 2.1. Задача №1 - Максимальное покрытие

--- src/main/kotlin/Task2.kt
+++ src/main/kotlin/Task2.kt
@@ -1,89 +1,72 @@
-import java.util.*
-
 const val moneyCoefficient = 100
-
 const val maxFreeRangeMastercardOrMaestro = 75_000 * moneyCoefficient
-
 const val commissionMastercardOrMaestroPercentCoefficient = 0.006
 const val commissionMastercardOrMaestroValue = 20 * moneyCoefficient
 const val commissionVisaOrMirPercentCoefficient = 0.0075
-const val commissionVisaOrMirPercentValue= 35 * moneyCoefficient
+const val commissionVisaOrMirPercentValue = 35 * moneyCoefficient
 
-fun main(){
+fun main() {
 
-    val scanner = Scanner(System.`in`)
+    val typeCard = TypeCard.MastercardOrMaestro
+    val amountPreviousTransfers = 10_000_000
+    val totalCurrentTransfers = 100_000
 
-    val typeCard = defineTypeCard(scanner)
-    val amountPreviousTransfers = defineAmountPreviousTransfers(scanner) * moneyCoefficient
-    val totalCurrentTransfers = defineTotalCurrentTransfers(scanner) * moneyCoefficient
-
-    printCommission(typeCard,amountPreviousTransfers, totalCurrentTransfers, scanner)
-    scanner.close()
+    printComission(typeCard, amountPreviousTransfers, totalCurrentTransfers)
 }
 
-fun printCommission(typeCard: TypeCard = TypeCard.VkPay,amountPreviousTransfers: Int = 0, totalCurrentTransfers: Int, scanner: Scanner){
+fun printComission(typeCard: TypeCard, amountPreviousTransfers: Int, totalCurrentTransfers: Int) {
+    val commission: Double
 
-    val totalCurrentTransfersRub = totalCurrentTransfers / moneyCoefficient
-
-    when(typeCard){
+    when (typeCard) {
         TypeCard.MastercardOrMaestro -> {
-
-            val total = amountPreviousTransfers + totalCurrentTransfers
-
-            when{
-                total > maxFreeRangeMastercardOrMaestro -> {
-                    if (amountPreviousTransfers - maxFreeRangeMastercardOrMaestro < 0){
-                        println("При переводе $totalCurrentTransfersRub руб. коммисия соствит ${((totalCurrentTransfers.toDouble()
-                                + amountPreviousTransfers - maxFreeRangeMastercardOrMaestro) * commissionMastercardOrMaestroPercentCoefficient
-                                + commissionMastercardOrMaestroValue) / moneyCoefficient} руб")
-                    } else{
-                        println("При переводе $totalCurrentTransfersRub руб. коммисия соствит ${(totalCurrentTransfers.toDouble() * commissionMastercardOrMaestroPercentCoefficient
-                                + commissionMastercardOrMaestroValue) / moneyCoefficient} руб")
-                    }
-                }
-                else -> {
-                    println("При переводе $totalCurrentTransfersRub руб. коммисия соствит 0 руб")
-                }
-            }
+            commission = calculateComissionMastercardAndMaestro(
+                amountPreviousTransfers,
+                totalCurrentTransfers,
+            )
         }
         TypeCard.VisaOrMir -> {
-            println("При переводе $totalCurrentTransfersRub руб. коммисия соствит ${(totalCurrentTransfers.toDouble() * commissionVisaOrMirPercentCoefficient
-                    + commissionVisaOrMirPercentValue) / moneyCoefficient} руб")
+            commission = calculateComissionVisaAndMir(totalCurrentTransfers)
         }
         else -> {
-            println("При переводе $totalCurrentTransfersRub руб. коммисия соствит 0 руб")
+            commission = 0.0
         }
     }
+    println("При переводе ${totalCurrentTransfers / moneyCoefficient} руб. коммисия соствит $commission руб")
 }
 
-fun defineTypeCard(scanner: Scanner): TypeCard{
-    println("""Выберите Тип карты/счёта
-        |1. Mastercard или Maestro;
-        |2. Visa или Мир;
-        |3. Vk Pay.
-    """.trimMargin())
+fun calculateComissionVisaAndMir(totalCurrentTransfers: Int): Double {
 
-    val userInput = scanner.nextInt()
-    return when(userInput){
-        1 -> TypeCard.MastercardOrMaestro
-        2 -> TypeCard.VisaOrMir
-        else -> TypeCard.VkPay
+    return (totalCurrentTransfers.toDouble() * commissionVisaOrMirPercentCoefficient
+            + commissionVisaOrMirPercentValue) / moneyCoefficient
+}
+
+
+fun calculateComissionMastercardAndMaestro(
+    amountPreviousTransfers: Int,
+    totalCurrentTransfers: Int,
+): Double {
+    val total = amountPreviousTransfers + totalCurrentTransfers
+    val commission: Double
+
+    when {
+        total > maxFreeRangeMastercardOrMaestro -> {
+            if (amountPreviousTransfers - maxFreeRangeMastercardOrMaestro < 0) {
+                commission = ((totalCurrentTransfers.toDouble()
+                        + amountPreviousTransfers - maxFreeRangeMastercardOrMaestro) * commissionMastercardOrMaestroPercentCoefficient
+                        + commissionMastercardOrMaestroValue) / moneyCoefficient
+            } else {
+                commission = (totalCurrentTransfers.toDouble() * commissionMastercardOrMaestroPercentCoefficient
+                        + commissionMastercardOrMaestroValue) / moneyCoefficient
+            }
+        }
+        else -> {
+            commission = 0.0
+        }
     }
+    return commission
 }
 
-fun defineAmountPreviousTransfers(scanner: Scanner): Int{
-    println("Введите сумму предыдущих переводов в этом месяце.")
-    val userInput = scanner.nextInt()
-    return userInput
-}
-
-fun defineTotalCurrentTransfers(scanner: Scanner): Int{
-    println("Введите сумму совершаемого перевода")
-    val userInput = scanner.nextInt()
-    return userInput
-}
-
-enum class TypeCard{
+enum class TypeCard {
     MastercardOrMaestro,
     VisaOrMir,
     VkPay
---
+++ src/test/kotlin/Task2KtTest.kt
@@ -0,0 +1,74 @@
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class Task2KtTest {
+
+    @Test
+    fun calculateComissionVisaAndMirTest(){
+
+        val totalCurrentTransfers = 1000 * moneyCoefficient
+
+        val result = calculateComissionVisaAndMir(totalCurrentTransfers)
+        assertEquals(42.5, result, 0.0000)
+    }
+
+    @Test
+    fun calculateComissionMastercardAndMaestroTestFreeLimitEnded(){
+
+        val amountPreviousTransfers = 100_000 * moneyCoefficient
+        val totalCurrentTransfers = 1000 * moneyCoefficient
+
+        val result = calculateComissionMastercardAndMaestro(amountPreviousTransfers, totalCurrentTransfers)
+        assertEquals(26.0, result, 0.0000)
+    }
+
+    @Test
+    fun calculateComissionMastercardAndMaestroTestFreeLimitNotEnded(){
+
+        val amountPreviousTransfers = 0
+        val totalCurrentTransfers = 1000 * moneyCoefficient
+
+        val result = calculateComissionMastercardAndMaestro(amountPreviousTransfers, totalCurrentTransfers)
+        assertEquals(0.0, result, 0.0000)
+    }
+
+    @Test
+    fun calculateComissionMastercardAndMaestroTestFreeLimitPartEnded(){
+
+        val amountPreviousTransfers = 74_500 * moneyCoefficient
+        val totalCurrentTransfers = 1000 * moneyCoefficient
+
+        val result = calculateComissionMastercardAndMaestro(amountPreviousTransfers, totalCurrentTransfers)
+        assertEquals(23.0, result, 0.0000)
+    }
+
+    @Test
+    fun printComissionTestMastercardOrMaestro(){
+
+        val typeCard = TypeCard.MastercardOrMaestro
+        val amountPreviousTransfers = 10_000_000
+        val totalCurrentTransfers = 100_000
+
+        val result = printComission(typeCard, amountPreviousTransfers, totalCurrentTransfers)
+    }
+
+    @Test
+    fun printComissionTestVisaOrMir(){
+
+        val typeCard = TypeCard.VisaOrMir
+        val amountPreviousTransfers = 10_000_000
+        val totalCurrentTransfers = 100_000
+
+        val result = printComission(typeCard, amountPreviousTransfers, totalCurrentTransfers)
+    }
+
+    @Test
+    fun printComissionTestVkPay(){
+
+        val typeCard = TypeCard.VkPay
+        val amountPreviousTransfers = 10_000_000
+        val totalCurrentTransfers = 100_000
+
+        val result = printComission(typeCard, amountPreviousTransfers, totalCurrentTransfers)
+    }
+}
